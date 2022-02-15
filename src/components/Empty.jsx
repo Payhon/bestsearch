e0a6443f026ed9c5338a731ae376db19f223c20c
payhon <payhon@163.com>
2022-02-15 10:20:36 +0800

    修改关键词提示

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 function Empty() {
 	const { keyword } = useParams();
-	return <div>There is no matching product of keyword '{keyword}'</div>;
+	return <div>There is no matching product of keyword '{keyword}',try 'hat'?</div>;
 }
 
 export default Empty;
